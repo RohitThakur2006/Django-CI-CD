4f4de0c5844a1b8db34c5fca8ba2ca4b39d0270a
mani1604 <manish_sharma1604@yahoo.com>
2026-05-13 21:56:47 +0530

    fixed env variable for vite

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_APP_API_URL || 'http://localhost:8000'
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+
+console.log(import.meta.env)
+console.log(import.meta.env.VITE_API_URL)
 
 export default API_BASE_URL
 
