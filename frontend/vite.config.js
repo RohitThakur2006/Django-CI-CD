8b196e64573ccfef28ba19326c3eec4c328dc89d
Rohit <rohit6thakur9@gmail.com>
2026-05-21 02:29:09 +0530

    fixed one error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['https://lms-frontend-v1-1nto.onrender.com'],
+    allowedHosts: ['lms-frontend-v1-1nto.onrender.com'],
   },
 })
